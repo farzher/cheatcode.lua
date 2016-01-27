7f145085a4a111069f9986880ada4fd502aa768c
Stephen Kamenar <stephenkamenar@gmail.com>
2016-01-27 01:53:02 -0500

    comments :camel:

--- cheatcode.lua
+++ cheatcode.lua
@@ -18,57 +18,58 @@ function Cheatcode:__call(codes_map)
     local i = 0 while true do i = i+1
       local char = key:sub(i,i)
 
-      -- {up} sets char='up' and increments i to skip over those chars next time
-        if char=='{' then
-          char = ''
-          while true do
-            i = i+1
-            local tmp_char = key:sub(i,i)
-            if tmp_char=='}' or tmp_char==nil then break end
-            char = char .. tmp_char
-          end
+      -- {up} sets char='up' and increments i to skip over these chars next time
+      if char=='{' then
+        char = ''
+        while true do
+          i = i+1
+          local tmp_char = key:sub(i,i)
+          if tmp_char=='}' or tmp_char==nil then break end
+          char = char .. tmp_char
         end
+      end
 
       -- This is the last char in our code
-        if i>=#key then
-          last_table[char] = callback
-          break
-        end
+      if i>=#key then
+        last_table[char] = callback
+        break
+      end
 
       -- Go deeper
-        if last_table[char]==nil then last_table[char] = {} end
-        last_table = last_table[char]
+      if last_table[char]==nil then last_table[char] = {} end
+      last_table = last_table[char]
     end
   end
 
   current_map = the_map
 end
 
--- Called everytime a key is pressed, gotta go fast
+-- Called everytime a key is pressed, gotta go fast!
+-- No loops! Runs in constant time! O(1)
 function Cheatcode.handle(char)
   if char=='space' then char = ' ' end
 
   -- What's next?
-    local next_table = current_map[char]
+  local next_table = current_map[char]
 
   -- If its empty, reset to the_map
   -- If we were trying deeper than the_map, resend their input
-    if next_table==nil then
-      local should_retry = current_map~=the_map
-      current_map = the_map
-      if should_retry then Cheatcode.handle(char) end
-      return
-    end
+  if next_table==nil then
+    local should_retry = current_map~=the_map
+    current_map = the_map
+    if should_retry then Cheatcode.handle(char) end
+    return
+  end
 
   -- We found a function! execute it and reset to the_map
-    if type(next_table)=='function' then
-      next_table()
-      current_map = the_map
-      return
-    end
+  if type(next_table)=='function' then
+    next_table()
+    current_map = the_map
+    return
+  end
 
   -- Goto next table
-    current_map = next_table
+  current_map = next_table
 end
 
 return setmetatable(Cheatcode, Cheatcode)
